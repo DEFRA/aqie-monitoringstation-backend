ba193fb36d709d9c1e6cf5e90ce48e07f4b657ee
Karthick Muthukrishnan <karthick.muthukrishnan@cognizant.com>
2025-07-08 11:50:31 +0100

    sonar change
    code change for sonar fix

--- src/api/location/helpers/get-nearest-location.js
+++ src/api/location/helpers/get-nearest-location.js
@@ -62,8 +62,7 @@ function getNearestLocation(
       const pollutantData = curr.pollutants[pollutant]
       const { value, startDate, endDate } = pollutantData
 
-      const isValuePositive = value > 0
-      
+      const isValuePositive = value > 0      
       const isEndDateValid = endDate === null || endDate > '2017-12-31'
       const isValueMissingWithStart = value === null && startDate !== null
       if (
