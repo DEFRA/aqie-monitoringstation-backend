e1b7c3ddc4a1dfe9b29fc23ed5a4d28022344afa
Karthick Muthukrishnan <karthick.muthukrishnan@cognizant.com>
2025-08-05 10:21:57 +0100

    aqd_728
    code changes for pen testing security header fix

--- src/api/location/controllers/location.js
+++ src/api/location/controllers/location.js
@@ -6,11 +6,23 @@ const osplaceController = {
   handler: async (request, h) => {
     const getmonitoringstation = await fetchmonitoringstation(request)
     const allowOriginUrl = config.get('allowOriginUrl')
+
     return h
       .response({ message: 'success', getmonitoringstation })
       .code(statusCodes.ok)
       .header('Access-Control-Allow-Origin', allowOriginUrl)
+      .header(
+        'Content-Security-Policy',
+        "default-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'none'; base-uri 'self'"
+      )
+      .header('Referrer-Policy', 'strict-origin-when-cross-origin')
+      .header('X-Content-Type-Options', 'nosniff')
+      .header('X-Frame-Options', 'DENY')
+      .header('X-XSS-Protection', '1; mode=block')
+      .header(
+        'Strict-Transport-Security',
+        'max-age=63072000; includeSubDomains; preload'
+      )
   }
 }
-
 export { osplaceController }
